a7bcfcc6658a2b05e944c909b7998926fe99fa8e
Freddy <92876568+Freddyreza53@users.noreply.github.com>
2023-08-09 22:01:49 -0500

    Initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+
+    return (
+        <>
+            <h1>About</h1>
+            <p>This is where you can go to learn about the app.</p>
+        </>
+    )
+}
+
+export default About;
---
+++ src/components/App/App.jsx
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import './App.css';
+import StudentForm from '../StudentForm/StudentForm.jsx';
+import StudentList from '../StudentList/StudentList.jsx';
+import About from '../About/About';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">GitHub Student List</h1>
+                </header>
+                <br/>
+                <nav>
+                    <ul>
+                        <li>
+                        <Link to='/home'>Home</Link>
+                        </li>
+                        <li>
+                        <Link to='/about'>About</Link>
+                        </li>
+                        <li>
+                        <Link to='/allStudents'>All Students</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Route path='/home'>
+                    <StudentForm />
+                </Route>
+
+                <Route path='/about'>
+                    <About />
+                </Route>
+
+                <Route path='/allStudents'>
+                    <p>Student list:</p>
+                    <StudentList />
+                </Route>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
